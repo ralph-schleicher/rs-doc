acf69dae315e4fdd3ef164b6c678aca3e1d8bbd1
Ralph Schleicher <rs@ralph-schleicher.de>
2023-04-22 10:13:55 +0000

    Use package rs-doc-user.

--- generate-doc.lisp
+++ generate-doc.lisp
@@ -1,19 +1,24 @@
 (ql:quickload :rs-doc)
 
-(let ((prefix (asdf:system-source-directory :rs-doc))
-      (doc (rs-doc:gather-doc
+(in-package :rs-doc-user)
+
+(defparameter *prefix* (asdf:system-source-directory "rs-doc"))
+
+(let ((doc (gather-doc
             :package :rs-doc
-            :symbols '(rs-doc:gather-doc
-                       rs-doc:generate-doc
-                       rs-doc:*text-width*
-                       rs-doc:*text-wrap*
-                       rs-doc:*text-indent*
-                       rs-doc:*html-template*
-                       rs-doc:*html-values*
-                       rs-doc:*html-resources*))))
-  (rs-doc:generate-doc :data doc
-                       :output-format :text
-                       :output (merge-pathnames #P"doc/rs-doc.txt" prefix))
-  (rs-doc:generate-doc :data doc
-                       :output-format :html
-                       :output (merge-pathnames #P"doc/rs-doc.html" prefix)))
+            :symbols '(get-doc
+                       gather-doc
+                       generate-doc
+                       *lambda-list-init-form*
+                       *text-width*
+                       *text-wrap*
+                       *text-indent*
+                       *html-template*
+                       *html-values*
+                       *html-resources*))))
+  (generate-doc :data doc
+                :output-format :text
+                :output (merge-pathnames #P"doc/rs-doc.txt" *prefix*))
+  (generate-doc :data doc
+                :output-format :html
+                :output (merge-pathnames #P"doc/rs-doc.html" *prefix*)))
